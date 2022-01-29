6e699f33683e47814e944da43684aa201d2a3021
Mickael R <94828882+Micka0675@users.noreply.github.com>
2022-01-29 21:28:56 +0100

    v2
    Update 2.0
    
    Add missing links
    Add form script for spawn input values at the bottom of index.html

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
 var light = document.getElementById("light");
+var comBox = document.getElementById("comBox");
+var envoi = document.getElementById("com");
+
+envoi.addEventListener('click', afficheInfo);
+
+function afficheInfo()
+{
+    var name = document.body.querySelectorAll(".info");
+    var commentaire = document.getElementById("commentaire");
+
+    console.log(name[0].value);
+    console.log(name[1].value);
+    console.log(name[2].value);
+    console.log(commentaire.value);
+    comBox.innerHTML = "<p id=\"vae\"> Vous avez entré : </p> <ul id=\"comsList\"><li>" + name[0].value + "</li><li>" + name[1].value + "</li><li>" + name[2].value + "</li><li>" + commentaire.value + "</li></ul>"
+
+
+}
+
+
+
 
 light.addEventListener('click', lightMode);
 
@@ -7,3 +28,4 @@ function lightMode()
     var element = document.body;
     element.classList.toggle("lightMode");
 }
+
