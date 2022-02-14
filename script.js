ff632d64fac19fe364216ef0cd8f02103e63346c
Mickael R <94828882+Micka0675@users.noreply.github.com>
2022-02-14 07:46:24 +0100

    v4
    UPDATE abilities section with multiple <select>

--- script.js
+++ script.js
@@ -7,8 +7,21 @@ var blocCv = document.getElementById("blocCv");
 var blocSp = document.getElementById("blocSp");
 var real = document.querySelectorAll(".real");
 var pop = document.querySelectorAll(".pop");
+var choix1 = document.getElementById("choix1");
+var choix2 = document.getElementById("choix2");
+var choix3 = document.getElementById("choix3");
+var opts = document.querySelectorAll(".opts");
+var txtMaq = document.getElementById("txtMaq");
 
-console.log(real);
+
+console.log(choix1);
+console.log(opts);
+console.log(txtMaq);
+
+
+choix1.addEventListener('change', popDiv);
+
+
 envoi.addEventListener('click', afficheInfo);
 
 blocPendu.addEventListener('mouseover',detail);
@@ -23,10 +36,165 @@ blocCv.addEventListener('mouseout',hide3);
 blocSp.addEventListener('mouseover',detail4);
 blocSp.addEventListener('mouseout',hide4);
 
+
 // for ( i = 0 ; i <= real[4] ; i++ )
 //     {
 //         real[i].addEventListener('onmouseover', detail);
 //     }
+function popDiv()
+{
+    var txtIntS = document.getElementById('txtIntS');
+    var txtIntW = document.getElementById('txtIntW');
+    var txtGest = document.getElementById('txtGest');
+    var txtBack = document.getElementById('txtBack');
+    var txtBase = document.getElementById('txtBase');
+    var txtSecu = document.getElementById('txtSecu');
+// for (j = 0 ; j < choix.length ; j++)
+    // {
+        // for (k = 0 ; k < opts.length ; k++)
+        // {
+    console.log(opts[1].value);
+    console.log(opts[2].value);
+    console.log(opts[3].value);
+    console.log(opts[4].value);
+    console.log(opts[5].value);
+    console.log(opts[6].value);
+    console.log(opts[7].value);
+
+
+    if (opts[0].selected == true)
+    {
+        txtMaq.style.display = "none";
+        txtIntS.style.display = "none";
+        txtIntW.style.display = "none";
+        txtGest.style.display = "none";
+        txtBack.style.display = "none";
+        txtBase.style.display = "none";
+        txtSecu.style.display = "none";
+    }
+    if (opts[1].selected == true)
+    {
+        txtMaq.style.display = "block";
+        txtIntS.style.display = "none";
+        txtIntW.style.display = "none";
+        txtGest.style.display = "none";
+        txtBack.style.display = "none";
+        txtBase.style.display = "none";
+        txtSecu.style.display = "none";
+    }
+    else
+    {
+        txtMaq.style.display = "none";
+    }
+    if (opts[2].selected == true)
+    {
+        choix2.style.display ="block";
+        choix2.addEventListener('change' , popDiv2);
+        
+        function popDiv2()
+        {
+            
+        
+            if (opts[5].selected == true)
+            {
+                txtIntS.style.display = "block";
+                txtIntW.style.display = "none";
+                txtGest.style.display = "none";
+                txtBack.style.display = "none";
+                txtBase.style.display = "none";
+                txtSecu.style.display = "none";
+            }
+            else
+            {
+                txtIntS.style.display = "none";
+            }
+            if (opts[6].selected == true)
+            {
+                txtIntS.style.display = "none";
+                txtIntW.style.display = "block";
+                txtGest.style.display = "none";
+                txtBack.style.display = "none";
+                txtBase.style.display = "none";
+                txtSecu.style.display = "none";
+            }
+            else
+            {
+                txtIntW.style.display = "none";
+            }
+            if (opts[7].selected == true)
+            {
+                txtIntS.style.display = "none";
+                txtIntW.style.display = "none";
+                txtGest.style.display = "block";
+                txtBack.style.display = "none";
+                txtBase.style.display = "none";
+                txtSecu.style.display = "none";
+            }
+            else
+            {
+                txtGest.style.display = "none";
+            }
+        }
+    }
+    else
+    {
+        choix2.style.display = "none";
+    }
+    if (opts[3].selected == true)
+    {
+        choix3.style.display ="block";
+        choix3.addEventListener('change' , popDiv3);
+        
+        function popDiv3()
+        {
+            
+        
+            if (opts[9].selected == true)
+            {
+                txtIntS.style.display = "none";
+                txtIntW.style.display = "none";
+                txtGest.style.display = "none";
+                txtBack.style.display = "block";
+                txtBase.style.display = "none";
+                txtSecu.style.display = "none";
+            }
+            else
+            {
+                txtBack.style.display = "none";
+            }
+            if (opts[10].selected == true)
+            {
+                txtIntS.style.display = "none";
+                txtIntW.style.display = "none";
+                txtGest.style.display = "none";
+                txtBack.style.display = "none";
+                txtBase.style.display = "block";
+                txtSecu.style.display = "none";
+            }
+            else
+            {
+                txtBase.style.display = "none";
+            }
+            if (opts[11].selected == true)
+            {
+                txtIntS.style.display = "none";
+                txtIntW.style.display = "none";
+                txtGest.style.display = "none";
+                txtBack.style.display = "none";
+                txtBase.style.display = "none";
+                txtSecu.style.display = "block";
+            }
+            else
+            {
+                txtSecu.style.display = "none";
+            }
+        }
+    }
+    else
+    {
+        choix3.style.display = "none";
+    }
+}
 
 function afficheInfo()
 {
@@ -102,6 +270,7 @@ function hide4()
 }
 
 
+    
 
 
 
